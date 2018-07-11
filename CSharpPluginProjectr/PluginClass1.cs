925b8cfecb72e8d95487cce81bcb750ddb9e643d
Ram kumar balrajan <ramkumarb@karyatech.com>
2018-07-11 06:41:59 -0700

    first commit

---
+++ CSharpPluginProjectr/PluginClass1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+
+namespace CSharpPluginProjectr
+{
+
+    
+    public class PluginClass1 : IPlugin
+    {
+
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+
+            try
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                //My stuff here
+                //var response = service.Execute(new WhoAmIRequest());
+
+                var lastName = entity.GetAttributeValue<string>("lastname");
+                if (!string.IsNullOrEmpty(lastName))
+                {
+                    entity["lastname"] = lastName.ToUpper();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new InvalidPluginExecutionException(e.Message);
+            }
+        }
+    }
+}
---
+++ CSharpPluginTestProject1/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using CSharpPluginProjectr;
+using FakeXrmEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace CSharpPluginTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var context = new XrmRealContext
+            {
+                ProxyTypesAssembly = typeof(PluginClass1).Assembly,
+                ConnectionStringName = "CRMConnectionString"
+            };
+            var executionContext = context.GetDefaultPluginContext();
+            var target = new Entity("contact")
+            {
+                ["lastname"] = "Power",
+                Id = Guid.NewGuid()
+            };
+            executionContext.MessageName = "Create";
+            executionContext.Stage = 20;
+            executionContext.PrimaryEntityId = target.Id;
+            executionContext.PrimaryEntityName = target.LogicalName;
+            executionContext.InputParameters = new ParameterCollection
+            {
+                new KeyValuePair<string, object>("Target", target)
+            };
+
+            context.ExecutePluginWith<PluginClass1>(executionContext);
+
+            Assert.AreEqual("POWER",
+                ((Entity)executionContext.InputParameters["Target"]).GetAttributeValue<string>("lastname"));
+        }
+    }
+}
